afdcf3eb1174291429aaa15c6c867e8477c2af5c
Seek4samurai <grvrawat999@gmail.com>
2022-06-14 16:39:27 +0530

    wallet setup

--- scripts/token_deploy.py
+++ scripts/token_deploy.py
@@ -6,5 +6,6 @@ token_name = "TOKEN"
 token_symbol = "TKN"
 
 def main():
-    account = accounts[0]
+    # account = accounts[0]
+    account = accounts.add(config["wallets"]["from_key"])
     erc20 = TokenERC20.deploy(initial_supply, token_name, token_symbol, {"from": account})
--- scripts/token_import_deploy.py
+++ scripts/token_import_deploy.py
@@ -6,5 +6,6 @@ token_name = "TOKEN"
 token_symbol = "TKN"
 
 def main():
-    account = accounts[0]
+    # account = accounts[0]
+    account = accounts.add(config["wallets"]["from_key"])
     erc20 = TokenImports.deploy(initial_supply, {"from": account})
